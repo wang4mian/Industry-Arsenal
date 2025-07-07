d44809eecf2c22b497cec608e926c583c7997ef6
wang4mian <hello@intelliexport.com>
2025-07-07 14:32:38 +0800

    Fix: 修改为server模式以支持API路由

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config'
+import node from '@astrojs/node'
 
 export default defineConfig({
-  output: 'static',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   site: 'https://osint-workstation.vercel.app'
 })
